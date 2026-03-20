0f4e985226f135917e21f4c39720f02469821eea
brilianeriko-95 <brilianeriko95@gmail.com>
2026-03-21 01:02:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const PHOTO_DRAFT_KEYS = {
 };
 
 // URL Google Apps Script Backend
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzFDztUKfkZr2wR-XnTMOHScU6kSfJqihCi7umKnhnSAy1bl3jN7lPDzJAEFFN2qFHXog/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwM23Q7AoFaRWBnFsHtXL_4b_pRZiDCbHXdt0jHm3drZyU87AJX-xMaPUW-PmJ8l5CoCQ/exec";
 
 // Fallback users untuk mode offline (legacy support)
 const OFFLINE_USERS = {
