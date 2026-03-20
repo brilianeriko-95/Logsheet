24015cfbe122848804269b07554d7d2f8d741246
brilianeriko-95 <brilianeriko95@gmail.com>
2026-03-21 01:02:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // ============================================
 // 1. KONFIGURASI & KONSTANTA
 // ============================================
-const APP_VERSION = '1.7.1';
+const APP_VERSION = '1.7.2';
 const APP_NAME = 'Turbine Logsheet Pro';
 
 const AUTH_CONFIG = {
